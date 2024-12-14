bdc4a2bf16abe956dfe9636925e8550976579606
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2024-12-14 19:03:00 +0500

    solved all taks of 2nd week :)

---
+++ week2/tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+
+    cin >> n >> m;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int cnt = 0;
+    int left = 0, right = n - 1;
+
+    while (left <= right) {
+        if (arr[left] + arr[right] <= m) {
+            ++cnt;
+            ++left;
+            --right;
+        } else {
+            ++cnt;
+            --right;
+        }
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
